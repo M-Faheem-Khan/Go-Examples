31292cc8167048f77c8f8a04a11a987c0d83f2d5
Faheem Khan <faheem5948@gmail.com>
2019-07-10 20:23:21 -0400

    Create hello_world.go

---
+++ src/hello_world.go
@@ -0,0 +1,17 @@
+/**
+ * @author Muhammad Faheem Khan
+ * @email faheem5948@gmail.com
+ * @create date 2019-07-10 20:21:28
+ * @modify date 2019-07-10 20:21:28
+ * @desc This is a simple Hello World Program written in go
+ */
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
